4003dd5e965be7e4207cc90dcb172907e3873de1
Priyanka Negi <negipriyanka2211@gmail.com>
2023-02-26 15:03:09 +0530

    Accordion

---
+++ src/data.js
@@ -0,0 +1,34 @@
+ const questions= [
+    {
+        id: 1,
+        question: `What is HTML?`,
+        answer: `HTML stands for Hyper Text Markup Language. HTML is the standard markup language for creating Web pages. HTML consists of a series of elements. HTML elements tell the browser how to display the content.HTML elements label pieces of content such as "this is a heading", "this is a paragraph", "this is a link", etc.`
+    },
+    {
+        id: 2,
+        question: `What is CSS?`,
+        answer: `CSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen, paper, or in other media. CSS saves a lot of work. It can control the layout of multiple web pages all at once. External stylesheets are stored in CSS files. CSS is the language we use to style a Web page.
+        `
+    },
+    {
+        id: 3,
+        question: `What is JavaScript?`,
+        answer: `JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997. JavaScript is the world's most popular programming language.JavaScript is the programming language of the Web.JavaScript is easy to learn.`
+    },
+    {
+        id: 4,
+        question: `What is React?`,
+        answer: `React is a JavaScript library for building user interfaces. React is used to build single-page applications.React allows us to create reusable UI components. React, sometimes referred to as a frontend JavaScript framework, is a JavaScript library created by Facebook. React is a tool for building UI components.`
+    },
+    {
+        id: 5,
+        question: `What is MERN?`,
+        answer: `MERN stands for MongoDB, Express, React, Node, after the four key technologies that make up the stack.
+        MongoDB — document database
+        Express(.js) — Node.js web framework
+        React(.js) — a client-side JavaScript framework
+        Node(.js) — the premier JavaScript web server`
+    }
+]
+
+export default questions;
